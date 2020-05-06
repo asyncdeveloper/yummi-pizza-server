4e5c7eea06931d92261c13ddcec745a1bc3fb4c8
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-05-06 17:43:08 +0100

    Implement feature to make an order

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'products' => [
+                'required',
+                'min:1',
+                'array',
+                function($attribute, $value, $fail) {
+                    $ids = array_keys($value);
+                    $menuCountWithinArrIDs = Menu::whereIn('id', $ids)->count();
+                    if ($menuCountWithinArrIDs != count($ids))
+                        return $fail($attribute.' is invalid.');
+
+                    return true;
+                }
+            ],
+            'products.*' => 'required|numeric|min:1',
+            'name' => 'required|string|min:3|max:191',
+            'address' => 'required|string|min:3|max:191',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([ 'errors' => $validator->errors() ], 422);
+        }
+
+        $data = $validator->validated();
+
+        $productIds = $data['products'];
+        $products = collect([]);
+        $total = 0;
+        foreach ($productIds as $product => $quantity) {
+            $item = Menu::find($product);
+            $total += $item->price * $quantity;
+            $products->push($item->only([ 'id', 'name', 'price' ]));
+        }
+
+        Order::create([
+            'products' => json_encode($products),
+            'name' => $data['name'],
+            'address' => $data['address'],
+            'total_cost' => $total
+        ]);
+
+        return response()->json([ 'message' => 'Order created successfully' ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::group([ 'namespace' => 'Api'], function(){
 
     Route::apiResource('menu', 'MenuController');
+    Route::apiResource('order', 'OrderController');
 
 });
